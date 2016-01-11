46e2b7ff4dbbf0a71274d34aeb9149d20212438d
jshuadvd <jshuadvd@icloud.com>
2016-01-10 17:51:24 -0800

    Set the scene and camera

---
+++ main.js
@@ -0,0 +1,4 @@
+// Set scene and camera
+var scene = new THREE.Scene();
+var aspectRatio = window.innerWidth / window.innerHeight;
+var camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
