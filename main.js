fdce4b0a995358a0a47cfde639ba5f430a250ad3
jshuadvd <jshuadvd@icloud.com>
2016-01-10 17:52:19 -0800

    Set the DOM

--- main.js
+++ main.js
@@ -2,3 +2,9 @@
 var scene = new THREE.Scene();
 var aspectRatio = window.innerWidth / window.innerHeight;
 var camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+
+// Set the DOM
+var renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth,window.innerHeight);
+renderer.setClearColor("#000000");
+document.body.appendChild(renderer.domElement);
