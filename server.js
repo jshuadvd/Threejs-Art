e4e43037fae62fa5fbeefba4667139eb33410663
jshuadvd <jshuadvd@icloud.com>
2016-01-14 20:28:16 -0800

    Added development branch

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 var express = require('express');
-var cors = require('cors');
+// var cors = require('cors');
 var path = require('path');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 
 var app = express();
 
 var port = process.env.PORT || 8000;
-
-app.use(bodyParser.text({ type: 'text/html' }));
-app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+// app.use(bodyParser.text({ type: 'text/html' }));
+// app.use(cors());
+
 app.use(express.static(path.join(__dirname + '/')));
 
 
