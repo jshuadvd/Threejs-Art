08844c0f2119e139d90b23e7f047ee4fd02054e4
Joshua David <jshuadvd@icloud.com>
2016-04-21 23:22:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
+
 var port = process.env.PORT || 8000;
 
 app.use(bodyParser.text({ type: 'text/html' }));
