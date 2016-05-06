8c825d6961154ebe2390e25d5024b08cd1265301
Joshua David <jshuadvd@icloud.com>
2016-05-05 22:16:05 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var scene, camera, renderer, aspectRatio;
 var stats;
 var composer, effect, clock;
 
-
 /* ==================== [ Audio Context ] ==================== */
 
 var ctx = new AudioContext();
