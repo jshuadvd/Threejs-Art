a0869c52ed317fcf533efda41ebddc3b426410f2
Joshua David <jshuadvd@icloud.com>
2016-06-25 22:51:41 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var stats;
 var composer, effect, clock;
 
 
+
 /* ==================== [ Audio Context ] ==================== */
 var ctx = new AudioContext();
 var audio = document.getElementById('player');
