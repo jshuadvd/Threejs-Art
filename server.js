d05242a00bb3e2c94e28f01d3cbe1c252e809cfe
Joshua David <jshuadvd@icloud.com>
2016-06-28 23:33:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 8000;
 
+
 app.use(bodyParser.text({ type: 'text/html' }));
 // app.use(cors());
 app.use(function(req, res, next) {
